1ddc1744892533caf34a10f692d4af8c04cfb672
lebrancconvas <lebrancconvas@gmail.com>
2021-11-23 00:59:54 +0700

    🔨 Edit Question.

---
+++ client/src/data/QuestionsData.js
@@ -0,0 +1,48 @@
+const QuestionsData = [{
+        "id": 1,
+        "category": "Social Media",
+        "difficulty": "Easy",
+        "type": "Multiple Choice",
+        "question": "What youtube channel is the highest subscriber youtube channel in the world? (Date Accorded: September 2021)",
+        "answers": ["Pewdiepie", "T-Series", "Blackpink", "Mr.Beast"],
+        "correct_answer": "T-Series"
+    },
+    {
+        "id": 2,
+        "category": "Social Media",
+        "difficulty": "Easy",
+        "type": "Multiple Choice",
+        "question": "What is the newest name of Facebook Application",
+        "answers": ["Meta", "Facebook", "Weibo", "Whatsapp"],
+        "correct_answer": "Facebook"
+    },
+    {
+        "id": 3,
+        "category": "Social Media",
+        "difficulty": "Medium",
+        "type": "Multiple Choice",
+        "question": "Who is the first non co-founder user of Facebook?",
+        "answers": ["Mark Zuckerberg", "Chris Hughes", "Arie Hasit", "Andrei Boros"],
+        "correct_answers": "Arie Hasit"
+    },
+    {
+        "id": 4,
+        "category": "Game",
+        "difficulty": "Easy",
+        "type": "Multiple Choice",
+        "question": "What is the first Nintendo Video Game?",
+        "answers": ["Super Mario Bros.", "Hanafuda", "EVR Race", "Donkey Kong"],
+        "correct_answer": "EVR Race"
+    },
+    {
+        "id": 5,
+        "category": "Programming",
+        "difficulty": "Easy",
+        "type": "Multiple Choice",
+        "question": "What is the programming language that is considered to be the lowest-level language?",
+        "answers": ["C", "C++", "Assembly", "COBOL"],
+        "correct_answer": "Assembly"
+    }
+]
+
+export default QuestionsData;
--- client/src/pages/Questions.js
+++ client/src/pages/Questions.js
@@ -1,9 +1,34 @@
-import React from 'react'; 
+import React from "react";
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import {useSelector} from "react-redux"; 
+import QuestionsData from "../data/QuestionsData";  
 
-const Questions = () => (
-	<div>
-		<h1>Questions.</h1>  
-	</div>
-); 
+const Questions = () => {
+	const {question_category, question_difficulty, question_type, amount_of_question} = useSelector(State => State); 
+	console.log(amount_of_question); 
+	return (
+  <div>
+		<Box> 
+			<Typography variant="h3">Question 1</Typography> 
+			<Typography variant="h5" mt={3}>{QuestionsData[0].question}</Typography>  
+			<Box mt={2}>
+				<Button variant="contained">{QuestionsData[0].answers[0]}</Button> 
+			</Box>
+			<Box mt={2}>
+				<Button variant="contained">{QuestionsData[0].answers[1]}</Button>  
+			</Box>
+			<Box mt={2}>
+				<Button variant="contained">{QuestionsData[0].answers[2]}</Button>  
+			</Box>
+			<Box mt={2}>
+				<Button variant="contained">{QuestionsData[0].answers[3]}</Button> 
+			</Box>
+			<Box mt={5}>
+				Score: 1 / 6 
+			</Box> 
+		</Box>
+  </div>
+)};
 
-export default Questions; 
+export default Questions;
