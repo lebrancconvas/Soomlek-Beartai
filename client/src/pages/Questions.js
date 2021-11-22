292b296d479d812de3ec42f82a8f012133913dbb
lebrancconvas <lebrancconvas@gmail.com>
2021-11-23 06:56:38 +0700

    🔨 Add Random for get the question randomly.

--- client/src/pages/Questions.js
+++ client/src/pages/Questions.js
@@ -1,34 +1,46 @@
 import React from "react";
 import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import {useSelector} from "react-redux"; 
-import QuestionsData from "../data/QuestionsData";  
+import { useSelector } from "react-redux";
+import QuestionsData from "../data/QuestionsData";
+
+const getRandomInt = max => {
+	return Math.floor(Math.random() * Math.floor(max)); 
+}
 
 const Questions = () => {
-	const {question_category, question_difficulty, question_type, amount_of_question} = useSelector(State => State); 
-	console.log(amount_of_question); 
-	return (
-  <div>
-		<Box> 
-			<Typography variant="h3">Question 1</Typography> 
-			<Typography variant="h5" mt={3}>{QuestionsData[0].question}</Typography>  
-			<Box mt={2}>
-				<Button variant="contained">{QuestionsData[0].answers[0]}</Button> 
-			</Box>
-			<Box mt={2}>
-				<Button variant="contained">{QuestionsData[0].answers[1]}</Button>  
-			</Box>
-			<Box mt={2}>
-				<Button variant="contained">{QuestionsData[0].answers[2]}</Button>  
-			</Box>
-			<Box mt={2}>
-				<Button variant="contained">{QuestionsData[0].answers[3]}</Button> 
-			</Box>
-			<Box mt={5}>
-				Score: 1 / 6 
-			</Box> 
-		</Box>
-  </div>
-)};
+  const {
+    question_category,
+    question_difficulty,
+    question_type,
+    amount_of_question,
+  } = useSelector((State) => State);
+  console.log(question_category, question_difficulty, question_type, amount_of_question); 
+	const numberQuestion = getRandomInt(QuestionsData.length); 
+  return (
+    <div>
+      <Box>
+        <Typography variant="h3"> Question 1 </Typography> 
+        <Typography variant="h5" mt={3}>
+           
+          {QuestionsData[numberQuestion].question} 
+        </Typography> 
+        <Box mt={2}>
+          <Button variant="contained"> {QuestionsData[numberQuestion].answers[0]} </Button> 
+        </Box> 
+        <Box mt={2}>
+          <Button variant="contained"> {QuestionsData[numberQuestion].answers[1]} </Button> 
+        </Box> 
+        <Box mt={2}>
+          <Button variant="contained"> {QuestionsData[numberQuestion].answers[2]} </Button> 
+        </Box> 
+        <Box mt={2}>
+          <Button variant="contained"> {QuestionsData[numberQuestion].answers[3]} </Button> 
+        </Box> 
+        <Box mt={5}>Score: 1 / 6 </Box> 
+      </Box> 
+    </div>
+  );
+};
 
 export default Questions;
