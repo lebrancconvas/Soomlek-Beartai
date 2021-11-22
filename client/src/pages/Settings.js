eb0a45e8f41f1c294731c5411ddb50ac77af0a42
lebrancconvas <lebrancconvas@gmail.com>
2021-11-23 06:27:09 +0700

    ✨ Add Navigate to Questions page.

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -4,9 +4,11 @@ import { Box } from "@mui/system";
 import SelectField from "../components/SelectField";
 import TextFieldComp from "../components/TextFieldComp";
 import useAxios from "../hooks/useAxios";
+import {useNavigate} from "react-router-dom";  
 
 const Settings = () => {
   const { response, error, loading } = useAxios({ url: "/api_category.php" }); 
+	const navigate = useNavigate(); 
 
 	if(loading) {
 		return (
@@ -44,6 +46,7 @@ const Settings = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+		navigate("/questions"); 
   };
   return (
     <div>
