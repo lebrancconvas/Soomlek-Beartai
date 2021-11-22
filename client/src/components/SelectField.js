054aa099fcc420a5937e23ba2f10fdeede24b3fd
lebrancconvas <lebrancconvas@gmail.com>
2021-11-23 01:01:16 +0700

    🔨 Update Setting Field.

--- client/src/components/SelectField.js
+++ client/src/components/SelectField.js
@@ -1,27 +1,27 @@
-import React, {useState} from 'react';
-import {FormControl, InputLabel, MenuItem, Select} from '@mui/material';
-import {Box} from '@mui/system'; 
+import React, { useState } from "react";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { Box } from "@mui/system";
 
-const SelectField = props  => {
-		const {label} = props; 
-		const [value, setValue] = useState(""); 
-		const handleChange = () => {}; 
-		return (
-		<div>
-			<Box mt={3} width="100%">
-				<FormControl size="small" fullWidth>  
-					<InputLabel>
-						{label} 	
-					</InputLabel> 
-					<Select value={value} label={label} onChange={handleChange}>
-						<MenuItem>Option 1</MenuItem>
-						<MenuItem>Option 2</MenuItem>
-						<MenuItem>Option 3</MenuItem> 
+const SelectField = (props) => {
+  const { label, options } = props;
+  const [value, setValue] = useState("");
+  const handleChange = () => {};
+  return (
+    <div>
+      <Box mt={3} width="100%">
+        <FormControl size="small" fullWidth>
+          <InputLabel> {label} </InputLabel>
+          <Select value={value} label={label} onChange={handleChange}>
+						{options.map(({id, name}) => (
+							<MenuItem value={id} key={id}> 
+								{name} 
+							</MenuItem>
+						))}	
 					</Select>
-				</FormControl>
-			</Box>
-		</div>
-)
-}
+        </FormControl>
+      </Box> 
+    </div>
+  );
+};
 
-export default SelectField; 
+export default SelectField;
--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,36 +1,70 @@
-import React from 'react'; 
-import {Typography, Button} from '@mui/material'; 
-import {Box} from '@mui/system';  
-import SelectField from '../components/SelectField'; 
-import TextFieldComp from '../components/TextFieldComp'; 
-import useAxios from '../hooks/useAxios'; 
+import React from "react";
+import { Typography, Button, CircularProgress } from "@mui/material";
+import { Box } from "@mui/system";
+import SelectField from "../components/SelectField";
+import TextFieldComp from "../components/TextFieldComp";
+import useAxios from "../hooks/useAxios";
 
 const Settings = () => {
-	const {response, error, loading} = useAxios({url: '/api_category'}); 
-	console.log(response); 
-	const handleSubmit = e => {
-		e.preventDefault(); 
-	}; 
-	return (
-	<div>
-		<Typography variant="h2" fontWeight="bold">
-			Beartai ITQ 
-		</Typography>
-		<Typography variant="h5">
-			Setting App. 
-		</Typography>
-		<form onSubmit={handleSubmit}>
-			<SelectField label="Category" />  
-			<SelectField label="Difficulty" />
-			<SelectField label="Type" />
-			<TextFieldComp />  
-			<Box mt={3} width="100%"> 
-				<Button fullWidth variant="contained" type="submit"> 
-					Get Started. 
-				</Button>
-			</Box>
-		</form>
-	</div>
-)}
+  const { response, error, loading } = useAxios({ url: "/api_category.php" }); 
 
-export default Settings; 
+	if(loading) {
+		return (
+			<Box mt={20}> 
+				<CircularProgress /> 
+			</Box>
+		)
+	}
+
+	if(error) {
+		return (
+			<Typography variant="h6" mt={20} color="red"> 
+				Some Went Wrong. 	
+			</Typography>
+		)
+	}
+
+	const categoryOptions = [
+		{id: "gadget", name: "Gadget"},
+		{id: "social", name: "Social Media"},
+		{id: "programming", name: "Programming"},
+		{id: "game", name: "Video Game"},
+		{id: "app", name: "Application"}  
+	] 
+	
+	const difficultyOptions = [
+		{id: "easy", name: "Easy"}, 
+		{id: "medium", name: "Medium"}, 
+		{id: "hard", name: "Hard"} 
+	]
+	const typeOptions = [
+		{id: "multiple", name: "Multiple Choice."}, 
+		{id: "boolean", name: "True / False"} 
+	] 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+			<img src="./assets/beartai_logo.png" alt="beartai" /> 
+      <Typography variant="h2" fontWeight="bold">
+        Beartai ITQ
+      </Typography>
+      <Typography variant="h5">Setting App. </Typography>
+      <form onSubmit={handleSubmit}>
+        <SelectField options={categoryOptions} label="Category" /> 
+        <SelectField options={difficultyOptions} label="Difficulty" />
+        <SelectField options={typeOptions} label="Type" /> 
+        <TextFieldComp />
+        <Box mt={3} width="100%">
+          <Button fullWidth variant="contained" type="submit">
+            Get Started.
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
+};
+
+export default Settings;
