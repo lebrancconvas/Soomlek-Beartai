4acb1be7adfa0d3d1420d9c367308a08298346dd
lebrancconvas <lebrancconvas@gmail.com>
2021-11-23 06:15:39 +0700

    ✨ Map TextField Value to be value when it's selected.

--- client/src/components/SelectField.js
+++ client/src/components/SelectField.js
@@ -1,24 +1,44 @@
 import React, { useState } from "react";
 import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import { Box } from "@mui/system";
+import {useDispatch} from 'react-redux'; 
+import {handleCategoryChange, handleDifficultyChange, handleTypeChange} from '../redux/actions'; 
 
 const SelectField = (props) => {
   const { label, options } = props;
+  const dispatch = useDispatch(); 
   const [value, setValue] = useState("");
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    setValue(e.target.value); 
+    switch(label) {
+      case "Category": 
+        dispatch(handleCategoryChange(e.target.value)); 
+        break;
+      case "Difficulty": 
+        dispatch(handleDifficultyChange(e.target.value));
+        break;
+      case "Type": 
+        dispatch(handleTypeChange(e.target.value));
+        break; 
+      default: 
+        return; 
+    } 
+  };
   return (
     <div>
       <Box mt={3} width="100%">
         <FormControl size="small" fullWidth>
-          <InputLabel> {label} </InputLabel>
+          <InputLabel> {label} </InputLabel> 
           <Select value={value} label={label} onChange={handleChange}>
-						{options.map(({id, name}) => (
-							<MenuItem value={id} key={id}> 
-								{name} 
-							</MenuItem>
-						))}	
-					</Select>
-        </FormControl>
+             
+            {options.map(({ id, name }) => (
+              <MenuItem value={id} key={id}>
+                 
+                {name} 
+              </MenuItem>
+            ))} 
+          </Select> 
+        </FormControl> 
       </Box> 
     </div>
   );
