3999575383ad6a34b37a71c2a1940a0c5ac1046e
lebrancconvas <lebrancconvas@gmail.com>
2021-11-23 06:26:27 +0700

    ✨ Map Textfield Value to be the exact value.

--- client/src/components/TextFieldComp.js
+++ client/src/components/TextFieldComp.js
@@ -1,18 +1,29 @@
-import React from 'react'; 
-import {FormControl, TextField} from '@mui/material'; 
-import {Box} from '@mui/system'; 
+import React from "react";
+import { FormControl, TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import { useDispatch } from "react-redux";
+import {handleAmountChange} from '../redux/actions'; 
 
 const TextFieldComp = () => {
-	const handleChange = () => {}; 
-	return (
-		<div>
-			<Box mt={3} width="100%"> 
-				<FormControl fullWidth size="small"> 
-					<TextField onChange={handleChange} variant="outlined" label="Amount of Question." type="number" size="small" />  
-				</FormControl>
-			</Box>
-		</div>
-	);
+	const dispatch = useDispatch(); 
+  const handleChange = e => {
+		dispatch(handleAmountChange(e.target.value)); 
+	};
+  return (
+    <div>
+      <Box mt={3} width="100%">
+        <FormControl fullWidth size="small">
+          <TextField
+            onChange={handleChange}
+            variant="outlined"
+            label="Amount of Question."
+            type="number"
+            size="small"
+          />
+        </FormControl>
+      </Box>
+    </div>
+  );
 };
 
-export default TextFieldComp; 
+export default TextFieldComp;
