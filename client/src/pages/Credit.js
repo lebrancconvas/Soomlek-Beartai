821186a0a93297f8daa84d748fa1fc9cb17fb2db
lebrancconvas <lebrancconvas@gmail.com>
2021-11-23 15:20:25 +0700

    ✨ Add Back Button to The First Page.

--- client/src/pages/Credit.js
+++ client/src/pages/Credit.js
@@ -1,54 +1,62 @@
 import React from 'react'; 
-import {Typography} from '@mui/material'; 
+import {Typography, Button} from '@mui/material'; 
 import {Box} from '@mui/system'; 
+import {useNavigate} from 'react-router-dom'; 
 
 const Credit = () => {
+	const navigate = useNavigate(); 
+	const changeBack = () => {
+		navigate('/'); 
+	}
   return (
 		<div>
 			<Box>
 				<Typography variant="h2">
-					Project Credit. 
+					Credit. 
 				</Typography>
 			</Box>
 			<Box>
-				<Typography>
+				<Typography variant="h4" mt={3}>
 					Soomlek 
 				</Typography>
 			</Box>
 			<Box>
-				<Typography>
+				<Typography variant="h4">
 					Software Engineering Project 
 				</Typography>
 			</Box> 
 			<Box>
-				<Typography>
+				<Typography variant="h6" mt={5}> 
 					By 
 				</Typography>
 			</Box>
 			<Box>
-				<Typography>
-					Sikhret Jampasri 61050306  
+				<Typography variant="h6">
+					Sikhret Jampasri 61050306 (Software Tester) 
 				</Typography>
 			</Box>
 			<Box>
-				<Typography>
-					Poom Yimyuean 62050210 
+				<Typography variant="h6">
+					Poom Yimyuean 62050210 (Full-Stack Developer)  
 				</Typography>
 			</Box>
 			<Box>
-				<Typography>
-					Nophawit Mahanil 63050120   
+				<Typography variant="h6">
+					Nophawit Mahanil 63050120 (Project Manager)  
 				</Typography>
 			</Box>
 			<Box>
-				<Typography>
-					Department of Computer Science, Faculty of Science, <br />King Mongkut's Institute of Technology Ladkrabang(KMITL) 
+				<Typography variant="h6" mt={5}>  
+					Department of Computer Science, Faculty of Science, <br />King Mongkut's Institute of Technology Ladkrabang (KMITL)  
 				</Typography>
 			</Box> 
 			<Box>
-				<Typography>
+				<Typography variant="h6">  
 					Semester 1/2021  
 				</Typography>
+			</Box>
+			<Box mt={3}> 
+				<Button variant="contained" onClick={changeBack}>Back</Button> 
 			</Box>
 		</div>
 	)
